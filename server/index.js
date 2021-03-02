9b7a768b3bf8c07f53c44988674197bdf40efcd7
ywang113 <glariewang@gmail.com>
2021-03-02 13:34:43 +1100

    Version 1.0 Server Side
    All Todo Items finished

--- server/index.js
+++ server/index.js
@@ -5,6 +5,9 @@ const cors = require('cors');
 const uploadHandler = require('./helper/uploadHandler').uploadHandler
 const uploadRouter = require('./router/upload').uploadRouter
 const convertRouter = require('./router/convert').convert
+const routerPaths = require('./common/constants').server.path
+
+console.log(routerPaths)
 
 /**
  * Inilization
@@ -15,9 +18,9 @@ app.use(cors());
 /**
  * Routers
  */
-app.post('/upload',uploadHandler.any(), async (req,res) => {uploadRouter(req,res)} );
+app.post(`${routerPaths.upload}`,uploadHandler.any(), async (req,res) => {uploadRouter(req,res)} );
 
-app.post('/convert', uploadHandler.any(), async (req,res) => {convertRouter(req,res)} );
+app.post(`${routerPaths.convert}`, uploadHandler.any(), async (req,res) => {convertRouter(req,res)} );
 
 app.listen(8000, ()=>{
   console.log('app running on port 8000')
