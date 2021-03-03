4d969bd74e448bc643a4f6b75fdf64ae691d5b4b
ywang113 <glariewang@gmail.com>
2021-03-03 14:36:27 +1100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -88,7 +88,7 @@ export default function App() {
         setIsLoading('true')
         axios.post("http://3.24.137.31/convert",data)
         .then(res => {
-            console.log(res)
+            console.log(res.data)
             setRes(res.data);
 
             res.data.forEach( item => {
